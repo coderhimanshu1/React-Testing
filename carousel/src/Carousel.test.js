9b9bd53260d040f5c6a126f01f13e337a1776dd3
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-05-10 11:10:48 -0700

    adding test for left arrow

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getByAltText } from "@testing-library/react";
 import Carousel from "./Carousel";
 
 it("renders without crashing", () => {
@@ -11,26 +11,46 @@ it("matches snapshot", () => {
   expect(asFragment()).toMatchSnapshot();
 });
 
-it("works when you click on the right arrow", function () {
-  const { queryByTestId, queryByAltText } = render(<Carousel />);
+it("works when you click on the left arrow", () => {
+  const { getByTestId, getByAltText } = render(<Carousel />);
+
+  // expect the second image to show, but not the first
+  expect(
+    getByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).not.toBeInTheDocument();
+  expect(getByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // Click event on left arrow
+  const leftArrow = getByTestId("left-arrow");
+  fireEvent.click(leftArrow);
 
   // expect the first image to show, but not the second
   expect(
-    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+    getByAltText("Photo by Richard Pasquarella on Unsplash")
   ).toBeInTheDocument();
   expect(
-    queryByAltText("Photo by Pratik Patel on Unsplash")
+    getByAltText("Photo by Pratik Patel on Unsplash")
+  ).not.toBeInTheDocument();
+});
+
+it("works when you click on the right arrow", function () {
+  const { getByTestId, getByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(
+    getByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).toBeInTheDocument();
+  expect(
+    getByAltText("Photo by Pratik Patel on Unsplash")
   ).not.toBeInTheDocument();
 
   // move forward in the carousel
-  const rightArrow = queryByTestId("right-arrow");
+  const rightArrow = getByTestId("right-arrow");
   fireEvent.click(rightArrow);
 
   // expect the second image to show, but not the first
   expect(
-    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+    getByAltText("Photo by Richard Pasquarella on Unsplash")
   ).not.toBeInTheDocument();
-  expect(
-    queryByAltText("Photo by Pratik Patel on Unsplash")
-  ).toBeInTheDocument();
+  expect(getByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
